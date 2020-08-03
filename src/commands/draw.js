bb16a720551f8a917b9d841adce842808390e77a
Sergio Rodriguez <SRodriguez@add123.com>
2020-08-02 23:09:06 -0400

    Upgrade tile-bag

--- src/commands/draw.js
+++ src/commands/draw.js
@@ -6,11 +6,16 @@ import roll from '../utils/roll'
 const letterFormatter = v => `**${v}**`
 
 export default (msg, data) => {
-  const args = data.split(/\s+/)
+  const arg = data.split(/\s+/)[0]
 
   const bag = new SpellRPGBag()
-  const count = roll(args[0])
+  if (arg && !arg.match(/(^[0-9]+$)|(^[0-9]+d[0-9]+$)/)) {
+    msg.reply(`\`${arg}\` is not a valid argument.  Draw argument must be in the form \`###\` or \`##d##\`.`)
+    return
+  }
+
+  const count = roll(arg)
   const tiles = bag.draw(count).map(letterFormatter)
 
-  msg.channel.send(`<@!${msg.member.id}> drew ${count} letters: ${tiles.join(' ')}`)
+  msg.channel.send(`<@!${msg.member.id}> drew ${tiles.length} letters: ${tiles.join(' ')}`)
 }
